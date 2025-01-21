706c78cff2f7cdea129c3beafb910b354ede42c5
Corngaming1 <escobidojasper117@gmail.com>
2025-01-21 21:26:40 +0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -12,6 +12,11 @@ class RestaurantMenu:
   def get_price(self, name):
     return self.menu_items.get(name, None)
 
+def display_menu(self):
+print("Menu Items:")
+  for item, price in self.menu_items.items():
+    print (f"{item}: ${price:.2f}")
+
   def remove_item(self, name):
     if name in self.menu_items:
       del self.menu_items[name]
@@ -20,13 +25,18 @@ class RestaurantMenu:
 
 def main():
   menu = RestaurantMenu()
+
+  # Display Menu
+ menu.display_menu()
+   
   # Add initial menu items
   menu.add_item("Burger", 10.99)
   menu.add_item("Fries", 4.99)
 
-  
   menu.remove_item("Fries")
-  
+  # Display Menu
+ menu.display_menu()
+   
 
 if __name__ == "__main__":
 
